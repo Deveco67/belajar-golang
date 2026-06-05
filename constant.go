fdb4d5112a48dc4affb3a4090fcc40c4f3fa3da9
Deveco67 <dericoliunardo66@gmail.com>
2026-06-05 10:47:58 +0700

    Belajar Golang 05-06-2026

---
+++ constant.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const FirstName string = "Derico"
+	const LastName  string = "Liunardo"
+
+	fmt.Println(FirstName,LastName)
+}
---
+++ variable.go
@@ -0,0 +1,16 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	name := "Derico"
+// 	fmt.Println(name)
+
+
+// 	var (
+// 		FirstName string = "Derico"
+// 		LastName  string = "Liunardo"
+// 	)
+// 	fmt.Println(FirstName, LastName)
+	
+// }
